b07abad9d66b142705016eed7a79aaa2eb4a158a
Neil <neil2193@gmail.com>
2020-01-01 15:41:50 +0200

    Real Final!

--- Assets/Scripts/Model/GameStats.cs
+++ Assets/Scripts/Model/GameStats.cs
@@ -6,9 +6,9 @@ public static class GameStats
 {
     public static string WinningPlayer;
 
-    public static int[] WinningCounters = { 0,0}; //[0] is player's 0 number of wins, [1] is player's 1 number of wins
+    public static int[] WinningCounters = { 0,0}; //[0] is player's 0 amount of wins, [1] is player's 1 amount of wins
 
 
 
-    
+
 }
